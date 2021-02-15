26d733bd5719a6b5f117865a96191a380cbeafcf
Tomi Toivio <tomi.toivio@ukuli.fi>
2021-02-15 12:58:07 +0200

    Django

--- ukulisentryserver/blog/models.py
+++ ukulisentryserver/blog/models.py
@@ -23,3 +23,12 @@ class Post(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+from django.urls import reverse
+class Post(models.Model):
+    # ...
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
